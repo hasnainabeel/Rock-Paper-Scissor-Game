71ecb6d26ad86f14d71b44e34884deab45b6d08a
tarn.ptmtp@gmail.com <tarn.ptmtp@gmail.com>
2021-10-15 23:46:54 +0700

    features: display round, current score

--- RPS.py
+++ RPS.py
@@ -1,7 +1,6 @@
 import random
 import inquirer
 
-
 GAME_CHOICE = [
   inquirer.List('answer',
                 message="Choose your choice ..",
@@ -23,14 +22,18 @@ GAME_CHOICE_VALUE = {
   "QUIT" : "q"
 }
 
-print("*****Rock Paper Scissors*****\nInitial score given is 5 to both player and computer\nEnter 'Q' anytime to exit\n")
+print("*****Rock Paper Scissors*****\nInitial score given is 5 to both player and computer\n")
+
+def current_score(round, player_score, computer_score):
+    score_log = f"Round {round}\nPlayer Score: {player_score}\ncomputer_score: {computer_score}\n"
+    return score_log
 
 def start():
     player_score = 5
     computer_score = 5
-    i = 0
+    i = 1
 
-    while i <= 10:
+    while i <= 11:
         car = ["r", "p", "s"]
 
         comp = random.choice(car)
@@ -48,29 +51,36 @@ def start():
             print(f"Computer had SCISSORS\nYou Won the round\n")
             player_score += 1
             computer_score -= 1
+            print(current_score(i, player_score, computer_score))
         elif (person == "r" and comp == "p"):
             print(f"Computer had PAPER\nYou Lost the round\n")
             player_score -= 1
             computer_score += 1
+            print(current_score(i, player_score, computer_score))
         elif (person == "p" and comp == "r"):
             print(f"Computer had ROCK\nYou Won the round\n")
             player_score += 1
             computer_score -= 1
+            print(current_score(i, player_score, computer_score))
         elif (person == "p" and comp == "s"):
             print(f"Computer had SCISSORS\nYou Lost the round\n")
             player_score -= 1
             computer_score += 1
+            print(current_score(i, player_score, computer_score))
         elif (person == "s" and comp == "p"):
             print(f"Computer had PAPER\nYou Won the round\n")
             player_score += 1
             computer_score -= 1
+            print(current_score(i, player_score, computer_score))
         elif (person == "s" and comp == "r"):
             print(f"Computer had SCISSORS\nYou Lost the round\n")
             player_score -= 1
             computer_score += 1
+            print(current_score(i, player_score, computer_score))
         elif ((person == "r" and comp == "r") or (person == "p" and comp == "p") or (person == "s" and comp == "s")):
             print("Its a Tie\n")
             i -= 1
+            print(current_score(i, player_score, computer_score))
         else:
             print("Invalid input!!!\n")
 
